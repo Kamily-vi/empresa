7ed27fea57242fe8fe9cc5f30830717a1a20b82d
KAMILY VITORIA TOSTA <21192995@CFP127ALUNO.LOCAL>
2021-08-12 10:17:02 -0300

    concluído

--- PHL/viagem.php
+++ PHL/viagem.php
@@ -1,22 +1,39 @@
 <?php
+ if(isset($_POST["origem"]) && isset($_POST["destino"])
+     && isset($_POST["distanciaKm"]) && isset($_POST["pedagios"])){
 
-if (
-    isset($_POST["cidadeOrigem"]) && isset($_POST["cidadeDestino"])
-    && isset($_POST["distanciaKm"]) && isset($_POST["pedagios"])
-) {
+        $origem = $_POST["origem"];
+        $destino = $_POST["destino"];
+        $distanciaKm = $_POST["distanciaKm"];
+        $pedagios = $_POST["pedagios"];
 
-$cidadeOrigem = $_POST["cidadeOrigem"]
-$cidadeDestino = $_POST["cidadeDestino"]
-$distanciaKm = $_POST["distanciaKm"]
-$pedagios = $_POST["pedagios"]
-$total = $_POST
+        $valorFrete = 0;
+
+        $valorFrete = $distanciaKm * 6;
+
+        $valorFrete += $pedagios * 9.4;
+
+}else{
+     echo "Você não enviou os dados";
+    exit;
+}
 
 
-$valorpedagio = ($pedagio * 9,40)
-$valordistancia = ($distanciaKm * 6)
-$total = ($distanciaKm + $pedagio)
+?>
 
-echo "A viagem de $cidadeOrigem á $cidadeDestino irá custar o valor totaal de $total"
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css"/>
+    <title>Valor do frete</title>
+</head>
+<body>
+    <p class="resultado">A viagem de <?= $origem ?> à <?= $destino ?> irá custar o valor total de R$ <em><?= number_format($valorFrete,2,",", ".")?></em>.</p>
+</body>
+</html>
 
 
 
